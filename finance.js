11200ae615840c1d8eee545756356e236a68e0c3
sarthak agrawal <sharthak31@gmail.com>
2019-06-08 15:50:49 +0530

    update chat with summary

--- finance.js
+++ finance.js
@@ -2,7 +2,7 @@ var questionNum = 0;
 var spent=0;
 var balance=1000;
 var received=0;
-
+var summary = "";
 function show(){
 return balance;
 }
@@ -33,23 +33,34 @@ if(command == "spent"){
 spent = setOfcommand[1];
 console.log('spent');
 var finalBalance = spentMoney(spent);
-output.innerHTML = '<h1>Your balance left is' + (finalBalance) + '</h1>';
+output.innerHTML = '<h1>Your balance left is ' + (finalBalance) + '</h1>';
     document.getElementById("input").value = "";   
+    summary = summary  + 'You spend ' + spent +"<br>"
+    "\n";
 
     }
 
 if(command == "received"){
 received = setOfcommand[1];
 var finalBalance = receivedMoney(received);
-output.innerHTML = '<h1>Your balance left is' + (finalBalance) + '</h1>';
+output.innerHTML = '<h1>Your balance left is ' + (finalBalance) + '</h1>';
     document.getElementById("input").value = ""; 
+    summary = summary  + 'You received ' + received+"<br>" ;
+
 }
 if(command == "show"){
 var finalBalance = show();
-output.innerHTML = '<h1>Your balance left is' + (finalBalance) + '</h1>';
+output.innerHTML = '<h1>Your balance left is ' + (finalBalance) +  ' only </h1>';
     document.getElementById("input").value = ""; 
+    summary = summary  + 'You showed your balance ' +"<br>";
 }
-
+if(command == "summary"){
+    
+    output.innerHTML = '<h6>'+ summary + '</h6>';
+        document.getElementById("input").value = ""; 
+        // summary = summary  + 'You showed your balance ' +"\n";
+    console.log(summary);
+    }
 }
 
 $(document).keypress(function(event) {
